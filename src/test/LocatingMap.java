a862922c4e52c021170a08e269bdde8ceb46d9d7
masterpryvedennyi <58852541+masterpryvedennyi@users.noreply.github.com>
2020-02-29 15:55:51 -0500

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -4,6 +4,7 @@ public class LocatingMap {
     public static void main(String[] args){
     //
     System.out.println("Locating Map in progress...");
+     System.out.println("Bye Bye!");
     System.out.println("Map located!");
     //fetch + merge test
     }
